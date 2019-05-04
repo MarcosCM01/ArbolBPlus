200566e292462d42def17e331abdcd2c45dfc3ce
MarcosCM01 <40147926+MarcosCM01@users.noreply.github.com>
2019-05-04 01:40:27 -0600

    Insercion completada

--- Program.cs
+++ Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace PruebaArbolBPlus
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            ArbolBP<int> arbolito = new ArbolBP<int>();
+            NodoBP<int> nodito = new NodoBP<int>();
+            
+            string[] lineas = File.ReadAllLines("C: /Users/Marcos Andrés CM/Downloads/CSVPrueba.csv");
+            for (int i = 0; i < lineas.Length; i++)
+            {
+                arbolito.InsertarNodo(Convert.ToInt32(lineas[i]));
+            }
+            
         }
     }
 }
